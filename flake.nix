80e1953d3957c4221a16228bf696cf095a262f90
H1rono <hronok66@gmail.com>
2024-03-03 14:20:40 +0900

    :tada: Init nix flakes

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "定期リマインダー with GAS";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ nodejs_20 ];
+        };
+      });
+}
